431002ce5eafdffb679145e9efbf7ca1e04c0b91
“Harendra <harendrak007@gmail.com>
2024-07-29 00:18:02 +0530

    first commit

---
+++ first_ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "us-east-1"
+
+}
+resource "aws_instance" "instance-1" {
+  ami           = "ami-00c39f71452c08778"
+  instance_type = "t2.medium"
+  vpc_security_group_ids = ["sg-0fd03c13039a2e3d6"]
+  tags = {
+       "Name" = "EC2 Prod"
+     }
+}
