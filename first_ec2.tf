aeeedc9755170a4a5f240fa8f835a527bf2775cf
“Harendra <harendrak007@gmail.com>
2024-07-30 13:18:18 +0530

    changes tag 2th fikle

--- first_ec2.tf
+++ first_ec2.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "instance-1" {
   instance_type = "t2.medium"
   vpc_security_group_ids = ["sg-0fd03c13039a2e3d6"]
   tags = {
-       "Name" = "EC2 Prod Demo 1"
+       "Name" = "EC2 Prod Demo 2"
      }
 }
